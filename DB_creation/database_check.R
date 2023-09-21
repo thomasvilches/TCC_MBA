f81ab94c0754b0ddda2a839476ff1c5a8d6ff19e
thomasvilches <thomvilches@gmail.com>
2023-09-21 17:48:56 -0300

    pequeno problema quando le a base no R, encoding do texto

---
+++ DB_creation/database_check.R
@@ -0,0 +1,32 @@
+
+
+db <- conecta_base()
+dbListTables(db)
+
+
+read_any(db, "procedimentos")
+cidades <- read_any(db, "ibge_cidades")
+
+Encoding(cidades[["cidade"]]) <- "ASCII"
+
+declare_utf8 <- function(x) {
+  Encoding(x) <- "latin1"
+  x
+}
+
+cidades %>% mutate_if(is.character, declare_utf8) %>% pull() %>% 
+  iconv(from = "latin1", to = "UTF-8") %>% 
+  iconv(from = "UTF-8", to = "ASCII")
+
+codes <- stringi::stri_enc_mark(cidades$cidade)
+
+for(i in 1:length(codes))
+  cidades$cidade[i] <- iconv(cidades$cidade[i], from = codes[i], to = "ASCII")
+
+stringi::stri_enc_toutf8(cidades$cidade) %>% 
+  iconv(from = "UTF-8", to = "latin1")
+ 
+odbc::dbGetQuery(
+  db, paste0("SELECT * FROM public.", "ibge_cidades",";")
+)
+ 
